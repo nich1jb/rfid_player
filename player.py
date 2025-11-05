7767cd7392c36965ec3fbb68369d57f9c9f1a206
nich1jb <nich2jb@gmail.com>
2025-11-05 02:00:37 +0000

    prev song and play/pause

--- player.py
+++ player.py
@@ -32,9 +32,23 @@ for event in device.read_loop():
     if event.type == ecodes.EV_KEY:
         key_event = categorize(event)
 
+        if key_event.keycode == 'KEY_PLAYPAUSE' and key_event.keystate == key_event.key_down:
+            state = player.get_state()
+            if state == vlc.State.Playing:
+                player.pause()
+            elif state == vlc.State.Paused:
+                player.play()
+
         if key_event.keycode == 'KEY_NEXTSONG' and key_event.keystate == key_event.key_down:
             current_index = (current_index + 1) % len(SONG_LIST)
             play_song(current_index)
+
+        if key_event.keycode == 'KEY_PREVIOUSSONG' and key_event.keystate == key_event.key_down:
+            if player.get_time() < 2000 and current_index > 0:
+                current_index = (current_index - 1) % len(SONG_LIST)
+                play_song(current_index)
+            else:
+                player.set_time(0)
 
     # Optional: check if song finished automatically
     if player.get_state() not in {1, 2, 3, 4}:  # playing/paused states
